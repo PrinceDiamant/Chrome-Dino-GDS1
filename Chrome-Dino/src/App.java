9b374a0d232d6a1b27b35e309b2de4a07860c4c3
PrinceDiamant <76435044+PrinceDiamant@users.noreply.github.com>
2024-07-08 18:55:14 +0200

    Add files via upload

---
+++ Chrome-Dino/src/App.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+public class App {
+
+
+    public static void main(String[] args) throws Exception {
+
+        int boardWidth = 800;
+        int boardHeight = 550;
+
+        JFrame frame = new JFrame("Dino Game");
+        Dino game = new Dino();
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+}
